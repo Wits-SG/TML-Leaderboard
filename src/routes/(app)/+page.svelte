05cdacbc31b3883e49a7ad9637875ba76634df30
Orwellian <orwellian225@gmail.com>
2023-10-27 14:59:34 +0200

    add: Home Page

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,56 @@
+<div class="w-full max-h-full flex flex-col justify-center items-center p-10 gap-5">
+    <h1 class="text-4xl font-sans text-center w-1/3 border-b-4 border-black dark:border-white py-5">Home</h1>
+
+    <div class="w-full flex flex-row justify-evenly items-start gap-10">
+        <section class="w-80 flex flex-col gap-5">
+            <h2 class="text-2xl font-sans text-center w-full border-b-4 border-primary py-5">Description</h2>
+
+            <p>The Magic Library is 3D puzzle game inspired by escape rooms. The player is tasked with solving a series of puzzles scattered across the library.</p>
+
+            <p>
+                The Magic Library was built as our project for our Computer Graphics course. The goal of the project was to introduce us to ThreeJS
+                and higher level programming than WebGL / OpenGL but not quite at a game engine.
+            </p>
+
+            <p>Each puzzle was designed to showcase some feature or topic taught in the course, varying from shaders to graphical modelling and all things in between.</p>
+
+            <p>
+                As part of this, we put together a very rudimentary game engine built on top of 
+                <a href="https://threejs.org" class="text-sky-600 hover:text-sky-800 underline">ThreeJS</a> and 
+                <a href="https://github.com/kripken/ammo.js/" class="text-sky-600 hover:text-sky-800 underline">AmmoJS</a> called W3Ads (weeds).
+            </p>
+        </section>
+
+        <section class="max-w-1/2 flex flex-col gap-5">
+            <h2 class="text-2xl font-sans text-center w-full border-b-4 border-primary py-5">Puzzles</h2>
+
+            <div class="flex flex-row max-w-4xl gap-5">
+                <section class="flex flex-col gap-2">
+                    <h3 class="text-xl font-sans text-center w-full border-b-2 border-secondary py-2">The Office</h3>
+                    <p>Every library needs a wizard, and every wizard needs an office. Wizards always havs spare bits and bobs all over the place, maybe some of the objects have a crystal hidden behind them?</p>
+                </section>
+
+                <section class="flex flex-col gap-2">
+                    <h3 class="text-xl font-sans text-center w-full border-b-2 border-secondary py-2">The Music</h3>
+                    <p>Music can calm the soul and guide us through many complex subjects, but only if the musicians have someone to guide them.</p>
+                </section>
+
+                <section class="flex flex-col gap-2">
+                    <h3 class="text-xl font-sans text-center w-full border-b-2 border-secondary py-2">The Hearths</h3>
+                    <p>Libraries need a place to pull up a chair and read some <i>magic</i> books, and nothing beats a good fireplace. Except maybe a magic fireplace, it does however need to be lit first.<p>
+                </section>
+
+                <section class="flex flex-col gap-2">
+                    <h3 class="text-xl font-sans text-center w-full border-b-2 border-secondary py-2">The Mirror</h3>
+                    <p>Magic mirrors are a staple, they even show us whats really there, even if it doesn't seem like anything is. Now, how do get across this gap?</p>
+                </section>
+
+                <section class="flex flex-col gap-2">
+                    <h3 class="text-xl font-sans text-center w-full border-b-2 border-secondary py-2">The Chess</h3>
+                    <p>Chess is a game for the intellectually inclined, and our wizard here thinks himself very smart. So smart he's forgooten how his statues are stored...</p>
+                </section>
+            </div>
+
+        </section>
+    </div>
+</div>
